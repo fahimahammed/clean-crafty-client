5937a364846c05749857493c79c9387105048c54
fahimahammed <fahimahammed.cse@gmail.com>
2021-04-18 17:56:32 +0600

    done

--- src/components/Home/Blog/Blog.js
+++ src/components/Home/Blog/Blog.js
@@ -8,22 +8,22 @@ import './Blog.css';
 const blogData = [
     {
         _id: 1,
-        title: 'Confessions of an Office Cleaner',
-        date: '16 March, 2020',
-        post: 'Cleaning is the process of removing unwanted substances, such as dirt, infectious agents. Cleaning tasks such as mopping, waxing floors and vacuuming.',
+        title: 'Professional Carpet Cleaning',
+        date: '16 March, 2021',
+        post: 'Cleaning is the process of removing unwanted substances, such as dirt, infectious agents. Cleaning tasks such as dusting, mopping, waxing floors and vacuuming.',
         image: blogImg1
     },
     {
         _id: 2,
-        title: 'Confessions of an Office Cleaner',
-        date: '16 March, 2020',
+        title: 'Confessions of an Office Cleaner ',
+        date: '09 June, 2020',
         post: ' Allow yourself 2 1/2 hours a week to clean a two-bedroom home. You should also add an extra 1 hour a week when you decide to deep clean. If you have a typical',
         image: blogImg2
     },
     {
         _id: 3,
-        title: 'Confessions of an Office Cleaner',
-        date: '16 March, 2020',
+        title: 'Ensure Good Hygiene at the Office',
+        date: '29 March, 2020',
         post: 'Cleaning tasks such as dusting, sweeping floors and vacuuming. Refilling supplies, such as toilet paper. Cleaning spills, broken glass and other messes.',
         image: blogImg3
     }
